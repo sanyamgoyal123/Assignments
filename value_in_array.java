a6e0d06a7104db88084155c2aec507c27dc23ee3
sanyamgoyal123 <42338275+sanyamgoyal123@users.noreply.github.com>
2018-08-21 18:02:29 +0530

    Create value_in_array.java

---
+++ value_in_array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class array_2 {
+public static boolean fun(int[] arr, int i) {
+for(int x : arr) {
+if(i == x) {
+return true;
+}
+}
+return false;
+}
+public static void main(String args[]) {
+int[] arr = { 1,4,6,7,8,9,10 };
+  Scanner o = new Scanner(System.in);
+  int n = o.nextInt();
+  System.out.println(fun(arr , n));
+  }
+  }
+  
